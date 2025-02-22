16c18bff58a09f44b210ba4ebc2d197333a9beec
PooranaSelvan <pooranaselvan67@gmail.com>
2025-02-22 23:26:20 +0530

    axios instance

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-     origin: "*",
+     origin: "https://final-year-project-five-snowy.vercel.app/",
      credentials: true
 }));
 
